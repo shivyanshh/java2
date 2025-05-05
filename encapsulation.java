afe08bb56f6e1d93ff5c747ebd1728f88fb68b92
shivyanshh <shivyansh2835@gmail.com>
2025-05-05 21:58:36 +0530

    second update

--- encapsulation.java
+++ encapsulation.java
@@ -1,5 +1,27 @@
+class human{
+     private  int age;//=18;
+     private  String name ;//="shivyansh";
+
+     public void setage(int a){    //since it aint gonna returning anything its void
+        age = a;
+     }
+     public void setname(String a){    //since it aint gonna returning anything its void
+        name = a;
+     }
+
+     public int getage(){
+        return age;
+     }
+     public String getname(){
+        return name;
+     }
+}
 public class encapsulation{
-    public static void main(String[] args){
-        System.out.println("hello");
+    public static void main(String[] args){ // basically private is used for so tht nobody directly access it
+        human obj = new human();
+        obj.setage((15));
+        obj.setname("krrish");
+        System.out.println(obj.getage());
+        System.out.println(obj.getname());
     }
 }
