9158264d6d2159e505b0b5f4690575b96a4ed9f5
shivyanshh <shivyansh2835@gmail.com>
2025-05-08 12:26:19 +0530

    update...

---
+++ inheritance_un.java
@@ -0,0 +1,13 @@
+public class inheritance_un {
+}
+
+class idk extends Calc {
+    public int div(int n, int y){
+        return n/y;
+    }
+    public int multiply(int n, int y){
+        return n*y;
+    }
+
+
+}
